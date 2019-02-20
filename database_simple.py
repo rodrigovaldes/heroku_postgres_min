ede6eee3959d205e34010df3c77fceeaaf37de6c
rodrigovaldes <rodrigovaldes@uchicago.edu>
2019-02-20 12:48:55 -0600

    add string indicators

--- database_simple.py
+++ database_simple.py
@@ -5,13 +5,13 @@ DATABASE_URL = os.environ['DATABASE_URL']
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cur = conn.cursor()
-cur.execute('''
+cur.execute("""
             DROP TABLE IF EXISTS testing;
             CREATE TABLE testing (
                 id SERIAL PRIMARY KEY,
                 col_text varchar(200));
-            INSERT INTO testing (col_text) VALUES (hello);
-            ''')
+            INSERT INTO testing (col_text) VALUES ('hello');
+            """)
 # commit the changes to the database
 conn.commit()
 print('SQL code executed.')
