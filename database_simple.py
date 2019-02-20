866adbd88159792ef8d53c97eb500f930ab9af16
rodrigovaldes <rodrigovaldes@uchicago.edu>
2019-02-20 12:51:54 -0600

    testing output data

--- database_simple.py
+++ database_simple.py
@@ -5,13 +5,15 @@ DATABASE_URL = os.environ['DATABASE_URL']
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cur = conn.cursor()
-cur.execute("""
-            DROP TABLE IF EXISTS testing;
-            CREATE TABLE testing (
-                id SERIAL PRIMARY KEY,
-                col_text varchar(200));
-            INSERT INTO testing (col_text) VALUES ('hello');
-            """)
+# cur.execute("""
+#             DROP TABLE IF EXISTS testing;
+#             CREATE TABLE testing (
+#                 id SERIAL PRIMARY KEY,
+#                 col_text varchar(200));
+#             INSERT INTO testing (col_text) VALUES ('hello');
+#             """)
+cur.execute('''
+            SELECT * FROM testing;''')
 # commit the changes to the database
 conn.commit()
 print('SQL code executed.')
